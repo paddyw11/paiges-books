abe0869cfddbe51782eb460d9fe8d96810e37c1d
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-05-16 18:45:05 +0000

    updated admin fields for books and genre model

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Book, Genre
 
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class BooksAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku', 
+        'title',
+        'author',
+        'price',
+        'offer',
+    )
+
+    ordering = ('sku',)
+        
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Genre)
+admin.site.register(Book, BooksAdmin)
+admin.site.register(Genre, GenreAdmin)
