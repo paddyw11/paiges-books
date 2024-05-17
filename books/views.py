2ea197b8783554c743b44ef3f1f69a7c04d8f932
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-05-17 11:35:34 +0000

    added books details page functionality

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 # Create your views here.
 
@@ -12,3 +12,15 @@ def all_books(request):
     }
     
     return render(request, 'books/books.html', context)
+
+
+def book_detail(request, book_id):
+    """ A view to return individual book details """
+
+    book = get_object_or_404(Book, pk=book_id)
+
+    context = {
+    'book': book,
+    }
+
+    return render(request, 'books/book_detail.html', context)
