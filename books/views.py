6ec4fe161611564964e068e703cbdd9fffc0228c
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-05-18 20:55:21 +0000

    feat: added sorting by genre, author, price and offers attributes

--- books/views.py
+++ books/views.py
@@ -10,8 +10,24 @@ def all_books(request):
     books = Book.objects.all()
     query = None
     genres = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                books = books.annotate(lower_name=Lower('name'))
+            
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
+
+
         if 'genre' in request.GET:
             genre_name = request.GET['genre'].replace('_', ' ')
             # genre_name = request.GET['genre']
@@ -31,10 +47,13 @@ def all_books(request):
             queries = Q(title__icontains=query) | Q(short_description__icontains=query) | Q(author__icontains=query)
             books = books.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'books': books,
         'search_term': query,
         'current_genres': genres,
+        'current_sorting': current_sorting
     }
     
     return render(request, 'books/books.html', context)
