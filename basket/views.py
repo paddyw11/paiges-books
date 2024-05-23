b7c1b533f9dd231ccf1771360a23e2f0dd6d3870
Paddy Walsh <paddywalsh11@hotmail.co.uk>
2024-05-23 00:45:13 +0000

    style:Added css to success messages

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from books.models import Book
@@ -12,13 +12,14 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified book to the shopping basket """
 
-    book = Book.objects.get(pk=item_id)
+    book = get_object_or_404(Book, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Updated {book.title} quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
         messages.success(request, f'Added {book.title} to your basket')
@@ -29,13 +30,16 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ Adjusts the quantity of the specified book to the specified amount """
 
+    book = get_object_or_404(Book, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'Updated {book.title} quantity to {basket[item_id]}')
     else:
         basket.pop(item_id)
+        messages.success(request, f'Removed {book.title} from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -55,8 +59,13 @@ def update_quantity(request, item_id):
 
 
 def remove_item(request, item_id):
+    """ Removes the book from the basket."""
+
+    book = get_object_or_404(Book, pk=item_id)
     basket = request.session.get('basket', {})
     basket.pop(item_id, None)
+    messages.success(request, f'Removed {book.title} from your basket')
+
     request.session['basket'] = basket
     return redirect('view_basket')
 
@@ -71,12 +80,15 @@ def my_view(request):
 def remove_from_basket(request, item_id):
     """ Removes the item from the shopping basket """
     try:
+        book = get_object_or_404(Book, pk=item_id)
         basket = request.session.get('basket', {})
 
         basket.pop(item_id)
+        messages.success(request, f'Removed {book.title} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
