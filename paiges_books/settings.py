d8d667dacbeae9e7ee7f0b9980b390a2ef9c3d83
Patrick Walsh <paddywalsh11@hotmail.co.uk>
2024-09-24 20:25:38 +0000

    updated settings for AWS key

--- paiges_books/settings.py
+++ paiges_books/settings.py
@@ -210,7 +210,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'paiges-books'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_KEY')
     AWS_S3_CUSTOM_DOMAIN =F'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
