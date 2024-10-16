7e4c91fcb8949f73ad47fb8ad31937c8db00cb15
Patrick Walsh <paddywalsh11@hotmail.co.uk>
2024-10-16 22:59:41 +0000

    feat: added faqs page and about us page

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    path('', views.index, name='home')
-    
+    path('', views.index, name='home'),
+    path('about/', views.about_us, name='about_us'),
+    path('faqs/', views.faqs, name='faqs'),
 ]
